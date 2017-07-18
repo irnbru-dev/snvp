56e488c75a1c2d97bb864ff145bd5b2a0575aec7
irnbru-dev <darina.irnbru@gmail.com>
2017-07-18 18:28:35 +0300

    fancybox

--- src/js/scripts/script.js
+++ src/js/scripts/script.js
@@ -38,41 +38,13 @@ function equalHeights(elementsClass) {
 
 // Для включения JQuery UI Slider (on Catalog-filtr page)
 $( function() {
-  $( "#slider" ).slider({
-    range: "min"
-  });
-} );
+    $( "#slider" ).slider({
+        range: "min"
+    });
+});
 
 
-//MAP on contact page
-
-// ymaps.ready(function () {
-//     var myMap = new ymaps.Map('contact-map', {
-//             center: [55.751574, 37.573856],
-//             zoom: 9
-//         }, {
-//             searchControlProvider: 'yandex#search'
-//         }),
-
-//         myPlacemark = new ymaps.Placemark(myMap.getCenter(), 
-//         // {
-//             // hintContent: 'Собственный значок метки',
-//             // balloonContent: 'Это красивая метка'
-//         // }, 
-//         {
-//             // Опции.
-//             // Необходимо указать данный тип макета.
-//             // iconLayout: 'default#image',
-//             // Своё изображение иконки метки.
-//             iconImageHref: './img/',
-//             // Размеры метки.
-//             iconImageSize: [30, 42],
-//             // Смещение левого верхнего угла иконки относительно
-//             // её "ножки" (точки привязки).
-//             iconImageOffset: [-5, -38]
-//         }),
-
-//     myMap.geoObjects
-//         .add(myPlacemark);
-// });
-
+$("[data-fancybox]").fancybox({
+        loop: true,
+        toolbar: false
+    });
